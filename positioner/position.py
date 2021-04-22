5040d061f70c8ed0dde3099a2b6a45e0fa2bfd53
Michal Najman <michal.najman@databreakers.com>
2021-04-22 08:30:58 +0200

    feature: parse position from string

--- positioner/position.py
+++ positioner/position.py
@@ -32,3 +32,33 @@ class Order:
 
     def __repr__(self):
         return f"{self.order_type} {self.amount:1.3f}USD {self.option} at {self.price:1.3f}USD"
+
+    @classmethod
+    def make(cls, amount, order_type, option: Option):
+        return cls(float(amount), OrderType(order_type), option)
+
+    @classmethod
+    def parse(cls, string: str):
+        order_type, amount, option, _, price = string.split(" ")
+        order_type = order_type.split(".")[1]
+        amount = float(amount.replace("USD", ""))
+        price = float(price.replace("USD", ""))
+
+        if "BID" in option:
+            symbol = option.replace("-Side.BID", "")
+            side = "BID"
+        else:
+            symbol = option.replace("-Side.ASK", "")
+            side = "ASK"
+
+        option = Option.make(price, amount/price, side, symbol)
+        return cls.make(amount, order_type, option)
+
+
+def parse_orders(string: str):
+    orders_str = string.strip().replace("[", "").replace("]", "").split(",")
+    orders = []
+    for order in orders_str:
+        orders += [Order.parse(order)]
+
+    return orders
