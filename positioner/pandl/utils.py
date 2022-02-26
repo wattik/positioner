aba849b137fffb268aacd2e017abb6406a84ef02
Michal Najman <michal.najman@databreakers.com>
2022-02-26 12:00:29 +0100

    fix float error

--- positioner/pandl/utils.py
+++ positioner/pandl/utils.py
@@ -8,7 +8,7 @@ def ndarray_argument_convertor(f, pos, *args, **kwargs):
     if isinstance(args[pos], Number):
         new_args = list(args)
         new_args[pos] = np.asarray([new_args[pos]])
-        return f(*new_args, **kwargs)[0]
+        return float(f(*new_args, **kwargs)[0])
 
     elif isinstance(args[pos], list):
         new_args = list(args)
